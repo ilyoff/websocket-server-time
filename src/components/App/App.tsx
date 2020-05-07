f2a755531cac2d74e8210359a50603f935b6cf9b
ilyoff <ilyoff@gmail.com>
2020-05-07 23:23:00 +0300

    feat: create layout, remove extra files

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from "../Form/Form";
+import Layout from "../Layout/Layout";
+
+const App = (): JSX.Element => {
+    return (
+        <Layout>
+            <Form />
+        </Layout>
+    );
+};
+
+export default App;
