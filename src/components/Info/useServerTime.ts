c98f3e889b9ff0adea4678d67be69bdd77a2a174
ilyoff <ilyoff@gmail.com>
2020-05-11 02:27:13 +0300

    refactor: remove extra code

--- src/components/Info/useServerTime.ts
+++ src/components/Info/useServerTime.ts
@@ -1,26 +1,9 @@
-import {useCallback, useEffect, useRef, useState} from "react";
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import api from '../../api';
 
 export default function useServerTime() {
-    const didMount = useRef(true);
-    const [changeCallbackRef, updateChangeCallbackRef] = useState(false);
-    const getUrl = useCallback(() => api.subscribe(), [changeCallbackRef]);
-
-    useEffect(() => () => {
-        didMount.current = false;
-    }, []);
-
     const { lastJsonMessage, readyState } = useWebSocket(api.subscribe, {
-        shouldReconnect: () => didMount.current,
-        onClose() {
-            if (didMount.current) {
-                updateChangeCallbackRef(!changeCallbackRef);
-            }
-        },
-        onError(...e) {
-            console.log(e);
-        }
+        shouldReconnect: () => true,
     });
 
     return {
