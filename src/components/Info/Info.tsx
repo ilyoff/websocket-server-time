2991c471bbad9d392a85befb83191dd4d57b760c
ilyoff <ilyoff@gmail.com>
2020-05-10 17:53:53 +0300

    feat: integrate useServerTime to info page

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './Info.scss';
+import UserContext from "../../UserContext";
 import Box from "../Box/Box";
+import useServerTime from "./useServerTime";
+
+const formatTime = (timestamp: number): string => {
+    try {
+
+        const date = new Date(timestamp * 1000);
+
+        return date.toLocaleString('ru', {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+        });
+    } catch (e) {
+        return '';
+    }
+}
 
 const Info = (): JSX.Element => {
+    const { timestamp, connected } = useServerTime();
+    const { logout } = useContext(UserContext);
+
     return (
         <Box className="info">
             <p className="info__text">
                 Status:
-                <span className="info__status info__status--connected">
-                    connected
+                <span className={`info__status ${connected ? 'info__status--connected' : 'info__status--disconnected'}`}>
+                    {connected ? 'connected' : 'disconnected'}
                 </span>
             </p>
             <p className="info__date">
-                09.05.2020 18:00:21
+                {formatTime(timestamp)}
             </p>
 
-            <button className="info__logout">Logout</button>
+            <button className="info__logout" onClick={logout}>Logout</button>
         </Box>
     );
 };
