ede3f3370c9dc29dba292c4e78d5bbe7e665f402
ilyoff <ilyoff@gmail.com>
2020-05-11 02:39:26 +0300

    Updates

---
+++ precache-manifest.0b7731b84723aec41be865adfe70ebd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c90819ffdb904fe52e4b1a1536fa516",
+    "url": "/wss-server-time/index.html"
+  },
+  {
+    "revision": "a48b42ece008d97fad64",
+    "url": "/wss-server-time/static/css/main.118a104b.chunk.css"
+  },
+  {
+    "revision": "f6260eb5e219a35a3d16",
+    "url": "/wss-server-time/static/js/2.4e9e9b05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wss-server-time/static/js/2.4e9e9b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a48b42ece008d97fad64",
+    "url": "/wss-server-time/static/js/main.a24d3890.chunk.js"
+  },
+  {
+    "revision": "37965765b38c362b56de",
+    "url": "/wss-server-time/static/js/runtime-main.b6dc2bb8.js"
+  }
+]);
